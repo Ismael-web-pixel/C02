469a42dd61967e45de21a5939e48143ccda90c60
Godisouleymane <ssabiougodi@gmail.com>
2025-05-17 23:49:16 +0100

    premier commit

--- factorial.c
+++ factorial.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-int main(void)
-{
-    int nbre, factorial = 1;
-
-    scanf("%d", &nbre);
-
-    if (nbre > 0)
-    
-    for (int i = 1; i <= nbre; i++)
-    {
-        factorial *= i;
-    }
-
-    printf("Le factoriel de %d est : %d\n", nbre, factorial);
-
-    return 0;
-}
