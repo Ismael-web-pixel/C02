e5799dabd4ee383c80410193b369469730ff3288
Godisouleymane <ssabiougodi@gmail.com>
2025-05-18 00:11:32 +0100

    premier commit

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+#include <stdio.h>   // Inclusion de la bibliothèque standard pour les entrées/sorties
 
 int main() {
 
-    int a, b;
+    int a, b;   // Déclaration de deux variables entières
 
-    scanf("%d %d", &a, &b);
+    scanf("%d %d", &a, &b);   // Lecture de deux entiers saisis par l'utilisateur
     
     a = a + b;       // a devient 8
     b = a - b;      // b devient 5
     a = a - b;     // a devient 3
 
-    printf("Après échange: a = %d, b = %d\n", a, b);
+    printf("Après échange : a = %d, b = %d\n", a, b);   // Affiche les nouvelles valeurs après l'échange
 
-    return 0;
+    return 0;   // Fin du programme
 }
