a9cddc962909fdb284440a1a5ddc37128f2d281e
Godisouleymane <ssabiougodi@gmail.com>
2025-05-17 23:47:34 +0100

    premier commit

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,16 +1,1 @@
-#include <stdio.h>
 
-int main() {
-
-    int a, b;
-
-    scanf("%d %d", &a, &b);
-    
-    a = a + b;       // a devient 8
-    b = a - b;      // b devient 5
-    a = a - b;     // a devient 3
-
-    printf("Après échange: a = %d, b = %d\n", a, b);
-
-    return 0;
-}
