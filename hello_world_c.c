4d250eee372c9f1dba4165d3ddca96ded4af3780
Godisouleymane <ssabiougodi@gmail.com>
2025-05-18 02:03:38 +0100

    bien

--- hello_world_c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>    // Inclusion de la bibliothèque standard pour les entrées/sorties
-
-int main() {       // Point d'entrée du programme
-
-    printf("Hello, World!\n");    // Affiche le message "Hello, World!" suivi d'un saut de ligne
-
-    return 0;   // Indique que le programme s'est terminé correctement
-}
